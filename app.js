889a0d4f3caa4570b11a6eaddb612e9646c5a05d
MaelleT <maelle.taurand@ac-nantes.fr>
2017-10-10 22:08:52 +0200

    Ajout d'une image à la page d'accueil

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+app.use(express.static('public'));
+app.use('/static', express.static(__dirname + '/public'));
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
